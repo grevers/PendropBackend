8e36b5cee7702b14fe518f102ed8192392b730e5
Garrett Evers <grevers@bsu.edu>
2018-01-18 13:18:34 -0500

    removed REST api files

--- models/conversation.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose'),
-      Schema = mongoose.Schema;
-
-const ConversationSchema = new Schema({
-  participants: [{ type: Schema.Types.ObjectId, ref: 'User'}],
-});
-
-module.exports = mongoose.model('Conversation',ConversationSchema);
